5b272799393f0a3db606d82b011a85c12ce54bbf
adhizsrm <adhizsrm@gmail.com>
2025-05-08 20:24:20 +0530

    Passing object to props by using spread syntax

--- App.jsx
+++ App.jsx
@@ -7,18 +7,7 @@ export default function App() {
             <>
                 <Entry 
                     id={element.id} 
-                    img={
-                        {
-                            src:element.img.src, 
-                            alt:element.img.alt 
-                        }
-                    
-                    }
-                    title={element.title}
-                    country={element.country}
-                    googleMapsLink={element.googleMapsLink}
-                    dates={element.dates}
-                    text={element.text} 
+                    {...element}
                 />
             </>
             
